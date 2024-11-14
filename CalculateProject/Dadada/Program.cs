2d3f9515aa95b094bf2bfae9da699d56249d9b46
npo100y <mr.bcenpo100@gmail.com>
2024-11-14 13:51:44 +0300

    Add develop task

---
+++ CalculateProject/Dadada/Program.cs
@@ -0,0 +1,38 @@
+﻿float a = 0.0f;
+float b = 0.0f;
+string c = "";
+Console.WriteLine("Калькулятор (знак '=' для результата)\n\n");
+
+Console.WriteLine("Введите число:");
+b = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    Console.WriteLine("Введите знак: ");
+    c = Console.ReadLine();
+    if (c[0] == '-' || c[0] == '+' ||c[0] == '*' || c[0] == '/')
+    {
+        Console.WriteLine("Введите следующее число: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        switch (c[0])
+        {
+            case '+':
+                b += a;
+                break;
+            case '-':
+                b -= a;
+                break;
+            case '*':
+                b *= a;
+                break;
+            case '/':
+                b /= a;
+                break;
+        }
+    }
+    else if (c[0] == '=')
+    {
+        Console.WriteLine($"\nРезультат вычислений: {b}");
+        break;
+    }
+}
